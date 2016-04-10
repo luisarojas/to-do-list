7f6712f49c89afb11a70c6b788311c91a2b63424
Luisa Rojas <r.luisaf@gmail.com>
2016-04-10 19:02:59 -0400

    Created simple app

---
+++ catyears/catyears/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  catyears
+//
+//  Created by Luisa Rojas on 2016-04-10.
+//  Copyright © 2016 lrg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
