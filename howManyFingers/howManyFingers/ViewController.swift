a6c60cae3fb6a20d5496a7ceb5952066cf77e4b3
Luisa Rojas <r.luisaf@gmail.com>
2016-04-18 00:15:24 -0400

    Made How Many Fingers game app

---
+++ howManyFingers/howManyFingers/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  howManyFingers
+//
+//  Created by Luisa Rojas on 2016-04-17.
+//  Copyright © 2016 lrg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var userGuesss: UITextField!
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var score: UILabel!
+    
+    var scoreCount = 0
+    
+    @IBAction func guessButton(sender: AnyObject) {
+        
+        var randomNum = Int(arc4random_uniform(10))
+        
+        //convert guess from string to integer
+        var guessedInt = Int(userGuesss.text!)
+
+        score.text = "Score: \(scoreCount)"
+    
+        if guessedInt != nil {
+            // user guessed correctly
+            if guessedInt == randomNum {
+                
+                resultLabel.text = "Correct!"
+                scoreCount += 1
+                score.text = "Score: \(scoreCount)"
+                
+            } else {
+                
+                resultLabel.text = "Nope! It was \(randomNum)."
+            }
+            
+            
+        } else {
+            resultLabel.text = "Please enter a number 0 - 10"
+        }
+        
+        print(userGuesss.text)
+        
+    }
+    
+    @IBAction func resetButtonPressed(sender: AnyObject) {
+    
+        scoreCount = 0
+        score.text = "Score: \(scoreCount)"
+    
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
