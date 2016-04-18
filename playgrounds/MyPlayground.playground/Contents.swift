68a2adfd28c70578e3cfc59fb35cd6cbd8831c6f
Luisa Rojas <r.luisaf@gmail.com>
2016-04-17 22:58:10 -0400

    Learning arrays and dictionaries

--- playgrounds/MyPlayground.playground/Contents.swift
+++ playgrounds/MyPlayground.playground/Contents.swift
@@ -28,5 +28,32 @@ var longSentence = str + ", " + name
 
 var newString = name + " is \(int)"
 
+var arr = [1, 2, 3, 4, 5]
+arr.append(6)
+arr.removeAtIndex(2)
+print(arr, terminator:"")
 
+arr.removeRange(1...2)
 
+var newArr = [1, 2.0, "three"]
+
+var emptyArr:[Int] // Can only append integers
+
+var dict = ["name":"Luisa", "age":34, "gender":"female"]
+
+//print(dict["name"]) //Returns optional
+
+//unwrapping forces swift to return a value, if we're sure it will
+print(dict["name"]!, terminator:"") //Returns optional
+
+dict["hairColour"] = "brown"
+dict["age"] = 21 //could change to a string, like: "old"
+dict["age"] = nil // remove the value
+
+//adding a dictionary value to a string
+var name1 = dict["name"]
+var myString = "my name is \(name1!)"
+
+var name2 = "name"
+
+var myString1 = "my name is \(dict[name2]!)"
